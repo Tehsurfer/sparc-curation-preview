d0d8a6bed6bf8c766a1f6904d4564ee863371e4d
Keeran Balachandran <85910337+keeran97@users.noreply.github.com>
2021-11-09 11:02:11 +1300

    add 139, 117, 116, 119

--- curation/previews.js
+++ curation/previews.js
@@ -46,5 +46,65 @@ export default [
     type: 'plot',
     title: 'PR1705_Negddct',
     pennsieveId: 'package:5febabcf-6e49-4d6c-9163-d6e04fc811f8'
+  },
+  {
+    datasetId: 139,
+    type: 'plot',
+    title: 'Average per strain',
+    pennsieveId: 'package:b220ef4d-deba-4541-a707-315340c6eb46'
+  },
+  {
+    datasetId: 139,
+    type: 'plot',
+    title: 'Differential expression WistarVsSHR',
+    pennsieveId: 'package:bfd00749-f476-4859-868a-672275745f14'
+  },
+  {
+    datasetId: 117,
+    type: 'plot',
+    title: 'PR1643_Negddct',
+    pennsieveId: 'package:4b82a35a-7914-4080-85fa-f15fb6f984c0'
+  },
+  {
+    datasetId: 117,
+    type: 'plot',
+    title: 'PR1643 normalised gene counts',
+    pennsieveId: 'package:3730b83a-42d4-44b9-b90e-6f6fefa5b29b'
+  },
+  {
+    datasetId: 116,
+    type: 'plot',
+    title: 'PR1643-142 Samples Raw Gene Counts',
+    pennsieveId: 'package:7b1a5fe7-c8de-4c29-8c69-69b8186ccffe'
+  },
+    {
+    datasetId: 116,
+    type: 'plot',
+    title: 'PR1643 Normalised Gene Counts',
+    pennsieveId: 'package:f13a41ae-edd7-408c-89f5-477a23be9591'
+  },
+    {
+    datasetId: 119,
+    type: 'plot',
+    title: 'RAGP_4subs_final_negddct_matrix',
+    pennsieveId: 'package:dc315029-255d-4c41-86de-4b857d6149a9'
+  },
+    {
+    datasetId: 119,
+    type: 'plot',
+    title: 'RAGP_4subs_negdct',
+    pennsieveId: 'package:3df73fd7-2903-4880-80c6-ceb8f9b061ef'
+  },
+    {
+    datasetId: 119,
+    type: 'plot',
+    title: 'RAGP_4subs_negddct',
+    pennsieveId: 'package:156174dc-06e4-4b35-9ffa-2c4f01c91ba4'
+  },
+    {
+    datasetId: 119,
+    type: 'plot',
+    title: 'RAGP_4subs_raw_Ct',
+    pennsieveId: 'package:ac7c976a-459a-4d32-ae43-09cd89340480'
   }
 ]
