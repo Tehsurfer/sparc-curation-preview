46799c40d44bd8862d681d3922a9cb1703031dbf
Keeran Balachandran <85910337+keeran97@users.noreply.github.com>
2021-12-01 15:58:23 +1300

    test new metadata line (140)

--- curation/previews.js
+++ curation/previews.js
@@ -1901,8 +1901,7 @@ export default [
     datasetId: 140,
     type: 'plot',
     title: 'Normalised sequencing data',
-    metadata: {"version": "1.2.0", "type": "plot", "attrs": {"style": "heatmap", "logScale": true}},
-    layout: {margin: {t: 55, l: 200, r: 55, b: 200, pad: 4}},
+    metadata: {"version": "1.1.0", "type": "plot", "attrs": {"style": "heatmap", "logScale": true, "margin": {t: 55, l: 200, r: 55, b: 200, pad: 4}}},
     pennsieveId: 'package:f369aa8a-b182-42b7-9b28-89ca44365e6f',
     thumbnailId: 'package:f7102f30-6b65-4152-83d4-6bbb782f4d79'
   }
